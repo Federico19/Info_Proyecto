7b52131e04fb3deb7c72af6a66d42633742daf8c
Federico Ocampo <30559203+Federico19@users.noreply.github.com>
2022-12-21 17:51:16 -0300

    Update local.py

--- proyecto/proyecto/settings/local.py
+++ proyecto/proyecto/settings/local.py
@@ -1,15 +1,18 @@
 from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [FedeOcampo.pythonanywhere.com.]
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+	'default' : {
+		'ENGINE': 'django.db.backend.mysql',
+		'NAME': 'FedeOcampo$dbsql_1',
+		'USER': 'FedeOcampo',
+		'PASSWORD': '2y2son4!',
+		'HOST': 'FedeOcampo.mysql.pythonanywhere-services.com',
+	}
 }
