5807e140b49b0f578aa560f34a9a75a5b3f00e27
Federico Ocampo <30559203+Federico19@users.noreply.github.com>
2022-12-21 19:17:16 -0300

    Update local.py

--- proyecto/proyecto/settings/local.py
+++ proyecto/proyecto/settings/local.py
@@ -2,7 +2,7 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [FedeOcampo.pythonanywhere.com.]
+ALLOWED_HOSTS = ['FedeOcampo.pythonanywhere.com']
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
